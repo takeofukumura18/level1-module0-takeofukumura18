88c989e1e4d3ec858b2f08b9545abac37c3fb1b9
takeofukumura18 <league@WorthyMini-5.attlocal.net>
2018-06-26 11:59:34 -0700

    DAY 2

--- src/DuelingButtons.java
+++ src/DuelingButtons.java
@@ -28,32 +28,37 @@ public class DuelingButtons implements ActionListener {
 
 	private void createUI() {
 		// 1. Add the panel to the frame
-
+		frame.add(panel);
 		// 2. Make the frame visible
-
+		frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+	  leftButton.setText("Click me!");
 		// 4. Set the text of the rightButton to "Click me!"
-
+	  rightButton.setText("Click me!");
 		// 5. Add an action listener to the leftButton
-
+	  leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+	  rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+	  panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+	  panel.add(rightButton);
 		// 9. Pack the frame
-
+	  frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+	  frame.setTitle("Demanding Buttons");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
 		
-		
+		if(buttonPressed.equals(leftButton)) {
+			rightButton.setText("No, Click Me!");
+			rightButton.setPreferredSize(BIG);
+			leftButton.setText("Click Me!");
+			leftButton.setPreferredSize(SMALL);
+		}
 		/* If the buttonPressed was the leftButton....*/
 			// Set the text of the rightButton to "No, click Me!"
 			// Set the PREFERRED size of the rightButton to BIG
@@ -62,7 +67,12 @@ public class DuelingButtons implements ActionListener {
 		
 		
 		/* If the buttonPressed was the rightButton, do the opposite. */
-		
+		if(buttonPressed.equals(rightButton)) {
+			leftButton.setText("No, Click Me!");
+			leftButton.setPreferredSize(BIG);
+			rightButton.setText("Click Me!");
+			rightButton.setPreferredSize(SMALL);
+		}
 
 		frame.pack();
 	}
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -20,35 +20,61 @@ public class PhotoQuiz {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
-
+                int score = 0;
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+                String peyton="https://usatftw.files.wordpress.com/2016/02/peyton.jpg?w=1000&h=600&crop=1";
 		// 2. create a variable of type "Component" that will hold your image
-
+               Component holder;
 		// 3. use the "createImage()" method below to initialize your Component
-
+               holder = createImage(peyton);
+               
+               holder.setSize(500, 500);
 		// 4. add the image to the quiz window
-
+               quizWindow.add(holder);
 		// 5. call the pack() method on the quiz window
-
+               quizWindow.pack();
+               
+              
 		// 6. ask a question that relates to the image
-
+   			String theirs= JOptionPane.showInputDialog("Who is this quarterback(He is the G.O.A.T.)");
 		// 7. print "CORRECT" if the user gave the right answer
-
+               if(theirs.equals("Peyton Manning")) {
+            	   System.out.println("Correct! Go Broncos!");
+            	   score++;
+               }
 		// 8. print "INCORRECT" if the answer is wrong
-
+               if(!theirs.equals("Peyton Manning")) {
+            	   System.out.println("WRONG!");
+            	   
+               }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+               quizWindow.remove(holder);
 		// 10. find another image and create it (might take more than one line of code)
-
+               String tom="https://www.japantimes.co.jp/wp-content/uploads/2015/11/sp-nfl-a-20151201.jpg";
 		// 11. add the second image to the quiz window
-
+               Component holder1;
+               
+               holder1 = createImage(tom);
+               
+               quizWindow.add(holder1);
 		// 12. pack the quiz window
-
+               quizWindow.pack();
 		// 13. ask another question
-
+               
+               String theirs1=JOptionPane.showInputDialog("Who is this quarterback(he is the worst of all time)");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+               if(theirs1.equals("Tom Brady")) {
+            	   System.out.println("Correct! BRADY SUCKS!");
+            	   
+            	   
+               }
+               if(!theirs1.equals("Tom Brady")) {
+            	   System.out.println("WRONG! THIS QUARTEBACK SUCKS!");
+            	   score=score+0;
+            	  
+               }
+               
+               quizWindow.remove(holder1);
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
